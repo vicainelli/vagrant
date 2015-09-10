da37257610cc2407f4b75f5d1a408e2d996d0037
Vinicius Cainelli <vicainelli@gmail.com>
2015-09-10 20:10:03 -0300

    Add php5-mysql

--- .vagrant-provision/php.sh
+++ .vagrant-provision/php.sh
@@ -6,7 +6,7 @@ echo "=================================================================== Updati
 sudo apt-get update
 
 echo "==================================================================== Installing PHP"
-sudo apt-get install -y php5 libapache2-mod-php5 php5-mcrypt
+sudo apt-get install -y php5 libapache2-mod-php5 php5-mcrypt php5-mysql
 
 echo "================================================================= Restarting Apache"
 sudo service apache2 restart
